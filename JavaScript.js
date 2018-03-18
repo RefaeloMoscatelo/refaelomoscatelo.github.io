f6491b32fe5a521454d2b4d1f579a64770727d94
RefaeloMoscatelo <refaelomoscatelo@gmail.com>
2018-03-18 03:58:13 +0200

    sdfsdfrge
    dfsfg

--- JavaScript.js
+++ JavaScript.js
@@ -2,3 +2,12 @@
 script.src = '//code.jquery.com/jquery-1.11.0.min.js';
 document.getElementsByTagName('head')[0].appendChild(script);
 
+
+    var colors = ['blue', 'green', 'red', 'purple'];
+var index = 0;
+
+function myfunction() {
+    document.body.style.backgroundColor = colors[index];
+    index = (index + 1) % colors.length;
+}
+document.addEventListener('click', myfunction);
